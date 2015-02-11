76390d4f36dea5c7ffeba58bba52d993b5ba25da
James Estes <james.estes@gmail.com>
2015-02-11 09:10:58 -0700

    Attemt to detect and set java_home if not set

--- build.sh
+++ build.sh
@@ -3,10 +3,29 @@
 BASE=$(cd $(dirname $0) && pwd)
 
 VERSION=`cat pom.xml | grep '<version>' | head -1 | tail -1 | sed 's_.*<version>\([^<]*\)</version>.*_\1_'`
+POM_JAVA_VERSION=`grep '<source>' pom.xml | sed 's_.*<source>\([^<]*\)</source>.*_\1_'`
+
+if [ -z "${JAVA_HOME:-}" ]; then
+  if [ -x /usr/libexec/java_home ]; then
+    # MacOS. Fails with error if required version not supported.
+    FOUND_JAVA=`/usr/libexec/java_home -v ${POM_JAVA_VERSION}+`
+  elif [ -x "$(which javac)" ]; then
+    FOUND_JAVA=$(dirname $(dirname $(readlink -e $(which javac))))
+  fi
+  echo "JAVA_HOME not set, setting to ${FOUND_JAVA}"
+else
+  FOUND_JAVA=$JAVA_HOME
+fi
+
+CURR_JAVA_VERSION=`${FOUND_JAVA}/bin/java -fullversion 2>&1 | head -1 | awk -F\" '{print $2}'`
+if [[ "${CURR_JAVA_VERSION}" < "${POM_JAVA_VERSION}" ]]; then
+  echo "Build needs java ${POM_JAVA_VERSION}, but JAVA_HOME (${FOUND_JAVA}) is ${CURR_JAVA_VERSION}."
+  exit -1
+fi
 
 echo "Building Version [${VERSION}]"
 
-mvn -U -B clean package
+JAVA_HOME=${FOUND_JAVA} mvn -U -B clean package
 
 JARS=$(find "$BASE" -name "*-$VERSION-selfcontained.jar" | sed -e 's/^/  /')
 
